302a379367b656945c303b8d4a8b6cc299a1a4ff
tgold83 <ted.jonathan.gold@gmail.com>
2022-11-07 15:17:41 -0800

    add closre function for coinCounter
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>
    co-authored-by: Samuel Majerus <ladolego@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,38 +1,66 @@
-const coinCounter = (money) => {
-  //termination condition
-  if (isNaN(money)) {
-    return;
-  }
-  //base case
-  if (money == 0) {
-    return money;
-  }
-  //recursion 
-  else {
-    if (money/.25 >= 1)
-    {
-      const numQuarters = Math.floor(money/.25);
-      console.log("Quarters: " + numQuarters);
-      return coinCounter(money - numQuarters*.25);
+// const coinCounter = (money) => {
+//   //termination condition
+//   if (isNaN(money)) {
+//     return;
+//   }
+//   //base case
+//   if (money == 0) {
+//     return money;
+//   }
+//   //recursion 
+//   else {
+//     if (money/.25 >= 1)
+//     {
+//       const numQuarters = Math.floor(money/.25);
+//       console.log("Quarters: " + numQuarters);
+//       return coinCounter(money - numQuarters*.25);
+//     }
+//     if (money / .1 >= 1)
+//     {
+//       const numDime = Math.floor(money / .1);
+//       console.log("Dime: " + numDime);
+//       return coinCounter(money - numDime * .1);
+//     }
+//     if (money / .05 >= 1)
+//     {
+//       const numNickle = Math.floor(money / .05);
+//       console.log("Nickle :" + numNickle);
+//       return coinCounter(money - numNickle * .05);
+//     }
+//     if (money / .01 >= 1)
+//     {
+//       const numPenny = Math.floor(money / .01);
+//       console.log("Penny :" + numPenny);
+//       return coinCounter(money - numPenny * .01);
+//     }
+//   }
+// };
+// coinCounter(4.99);
+
+
+
+
+
+
+
+const coinCounter = (coinIndex) => {
+  const coinArray = [.25, .10, .05, .01];
+  return (totalMoney) => {
+    if (totalMoney == 0) {
+      return "no coin of this value";
     }
-    if (money / .1 >= 1)
-    {
-      const numDime = Math.floor(money / .1);
-      console.log("Dime: " + numDime);
-      return coinCounter(money - numDime * .1);
+    const money = totalMoney / coinArray[coinIndex];
+    if (money >= 1) {
+      //amount of specified coin
+      const coinNum = Math.floor(totalMoney / coinArray[coinIndex]);
+      console.log(coinNum)
+      //leftover money
+      return coinCounter(coinIndex + 1)(totalMoney - coinNum * coinArray[coinIndex]);
+    } 
+    if (money < 1){
+      const coinNum = 0;
+      return coinCounter(coinIndex + 1)(totalMoney - coinNum * coinArray[coinIndex]);
     }
-    if (money / .05 >= 1)
-    {
-      const numNickle = Math.floor(money / .05);
-      console.log("Nickle :" + numNickle);
-      return coinCounter(money - numNickle * .05);
-    }
-    if (money / .01 >= 1)
-    {
-      const numPenny = Math.floor(money / .01);
-      console.log("Penny :" + numPenny);
-      return coinCounter(money - numPenny * .01);
-    }
-  }
-};
-coinCounter(4.99);
+   }
+}
+coinCounter(0)(4.99); 
